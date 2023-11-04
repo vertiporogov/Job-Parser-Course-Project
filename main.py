93954de5291a63d9ccfa2b10d8676967bce6ccc2
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-11-04 23:26:47 +0300

    creating job_parser

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from src.classAPI import HeadHunterAPI, SuperJobAPI
 from src.classJSON import JSONSaver
 
-hh_api = HeadHunterAPI('администратор')
-f = hh_api.format(hh_api.get_vacancies())
+# hh_api = HeadHunterAPI('администратор')
+# f = hh_api.format(hh_api.get_vacancies())
 
-sj = SuperJobAPI('учитель')
-a = sj.get_vacancies()
-d = sj.format(a)
+# sj = SuperJobAPI('учитель')
+# a = sj.get_vacancies()
+# d = sj.format(a)
 
 # print(a)
 # print(hh_api.get_vacancies('python')[0])
 # f = hh_api.format(hh_api.get_vacancies())
 # print(f)
-s = JSONSaver()
-s.creating_json(d)
-g = s.get_vacancy()
-g = sorted(g)
+# s = JSONSaver()
+# s.creating_json(f)
+# g = s.set_salary(30000, 60000)
+# g = sorted(g)
 
-for i in g:
-    print(i)
-    print('*' * 100)
+# for i in g:
+#     print(i)
+#     print('*' * 100)
 # o = a.set_salary(30000, 60000)
 # print(o)
 # for i in o:
@@ -35,8 +35,46 @@ for i in g:
 
 def job_parser():
 
-    user_title = input('Введите название профессии ')
+    user_title = input('Введите название профессии по которой хотите узнать информацию ')
+
+    while True:
+
+        source_selection = int(input('Если хотите получить информацию с сайта HH.ru введите 1, если с сайта SuperJob.ru введите 2 '))
+
+        if source_selection == 1:
+
+            hh_api = HeadHunterAPI(user_title)
+            full_list_of_vacancies = hh_api.format(hh_api.get_vacancies())
+            instance_of_the_json = JSONSaver()
+            instance_of_the_json.creating_json(full_list_of_vacancies)
+            min_salary = int(input('Введите минимальную зарплату '))
+            max_salary = int(input('Введите максимальную зарплату '))
+            result = instance_of_the_json.set_salary(min_salary, max_salary)
+            for i in result:
+                print(i)
+                print('*' * 100)
+            break
+
+        elif source_selection == 2:
+
+            sj_api = SuperJobAPI(user_title)
+            full_list_of_vacancies = sj_api.format(sj_api.get_vacancies())
+            instance_of_the_json = JSONSaver()
+            instance_of_the_json.creating_json(full_list_of_vacancies)
+            min_salary = int(input('Введите минимальную зарплату '))
+            max_salary = int(input('Введите максимальную зарплату '))
+            result = instance_of_the_json.set_salary(min_salary, max_salary)
+            for i in result:
+                print(i)
+                print('*' * 100)
+            break
+
+        else:
+
+            print('Некорректно введен запрос\nДавайте сначала)')
+
+            continue
 
 
 if __name__ == "__main__":
-    pass
+    job_parser()
--- src/classJSON.py
+++ src/classJSON.py
@@ -22,7 +22,9 @@ class JSONSaver:
         for i in data:
             all_vacancy.append(Vacancy(i['name'], i['url'], i['salary_from'], i['salary_to'], i['requirement']))
 
-        return all_vacancy
+        sort_ = sorted(all_vacancy, reverse=True)
+
+        return sort_
 
     def set_salary(self, min_salary, max_salary):
 
@@ -30,7 +32,9 @@ class JSONSaver:
             data = json.load(f)
         all_vacancy = []
         for i in data:
-            if i['salary_from'] == min_salary and i['salary_to'] == max_salary:
+            if min_salary <= i['salary_from'] <= max_salary and min_salary <= i['salary_to'] <= max_salary:
                 all_vacancy.append(Vacancy(i['name'], i['url'], i['salary_from'], i['salary_to'], i['requirement']))
 
-        return all_vacancy
+        sort_ = sorted(all_vacancy, reverse=True)
+
+        return sort_
